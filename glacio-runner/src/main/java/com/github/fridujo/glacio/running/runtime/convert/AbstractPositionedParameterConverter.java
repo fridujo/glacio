2503fcec02f498b29bbaafdb852bc9e4dd2425a9
ledoyen <ledoyen.loic@gmail.com>
2018-12-03 17:22:09 +0100

    Add converter ? -> Optional<T>

--- glacio-runner/src/main/java/com/github/fridujo/glacio/running/runtime/convert/AbstractPositionedParameterConverter.java
+++ glacio-runner/src/main/java/com/github/fridujo/glacio/running/runtime/convert/AbstractPositionedParameterConverter.java
@@ -1,11 +1,15 @@
 package com.github.fridujo.glacio.running.runtime.convert;
 
 import com.github.fridujo.glacio.running.api.convert.Converter;
+import com.github.fridujo.glacio.running.api.convert.ParameterConverter;
+import com.github.fridujo.glacio.running.api.convert.ParameterConverterAware;
 import com.github.fridujo.glacio.running.api.convert.ParameterDescriptor;
 import com.github.fridujo.glacio.running.api.convert.SourceSet;
 import com.github.fridujo.glacio.running.api.convert.Value;
 
-public abstract class AbstractPositionedParameterConverter implements Converter {
+public abstract class AbstractPositionedParameterConverter implements Converter, ParameterConverterAware {
+
+    private ParameterConverter parameterConverter;
 
     @Override
     public final Value convert(SourceSet sourceSet, ParameterDescriptor parameterDescriptor) {
@@ -14,4 +18,20 @@ public abstract class AbstractPositionedParameterConverter implements Converter 
     }
 
     protected abstract Value convert(Value rawValue, ParameterDescriptor parameterDescriptor);
+
+    @SuppressWarnings("unchecked")
+    protected <T> T convertTo(Object value, Class<T> keyType) {
+        if (value == null || keyType == null || value.getClass() == keyType) {
+            return (T) value;
+        }
+        if (parameterConverter == null) {
+            throw new IllegalStateException("ParameterConverter must be set to perform parameter conversion");
+        }
+        return (T) parameterConverter.convert(SourceSet.fromRaw(value), new ParameterDescriptor(0, keyType, keyType, null));
+    }
+
+    @Override
+    public void setConverter(ParameterConverter parameterConverter) {
+        this.parameterConverter = parameterConverter;
+    }
 }
--- glacio-runner/src/main/java/com/github/fridujo/glacio/running/runtime/convert/DataTableToMapConverter.java
+++ glacio-runner/src/main/java/com/github/fridujo/glacio/running/runtime/convert/DataTableToMapConverter.java
@@ -4,15 +4,11 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import com.github.fridujo.glacio.model.DataTable;
-import com.github.fridujo.glacio.running.api.convert.ParameterConverter;
 import com.github.fridujo.glacio.running.api.convert.ParameterConverterAware;
 import com.github.fridujo.glacio.running.api.convert.ParameterDescriptor;
-import com.github.fridujo.glacio.running.api.convert.SourceSet;
 import com.github.fridujo.glacio.running.api.convert.Value;
 
 public class DataTableToMapConverter extends AbstractPositionedParameterConverter implements ParameterConverterAware {
-
-    private ParameterConverter parameterConverter = null;
 
     @Override
     protected Value convert(Value rawValue, ParameterDescriptor parameterDescriptor) {
@@ -21,7 +17,7 @@ public class DataTableToMapConverter extends AbstractPositionedParameterConverte
             .filterValue(DataTable::isSquare)
             .<DataTable>filterValue(d -> d.hasWidth(2))
             .filter(Map.class == parameterDescriptor.type || LinkedHashMap.class == parameterDescriptor.type)
-            .<DataTable>map(dataTable -> transform(
+            .<DataTable>mapPresent(dataTable -> transform(
                 dataTable,
                 parameterDescriptor.getTypeArgument(0),
                 parameterDescriptor.getTypeArgument(1)
@@ -37,21 +33,5 @@ public class DataTableToMapConverter extends AbstractPositionedParameterConverte
                 convertTo(row.getCells().get(1), valueType)
             ));
         return map;
-    }
-
-    @SuppressWarnings("unchecked")
-    private <T> T convertTo(String stringValue, Class<T> keyType) {
-        if (keyType == null || String.class == keyType) {
-            return (T) stringValue;
-        }
-        if (parameterConverter == null) {
-            throw new IllegalStateException("ParameterConverter must be set to perform parameter conversion");
-        }
-        return (T) parameterConverter.convert(SourceSet.fromRaw(stringValue), new ParameterDescriptor(0, keyType, keyType, null));
-    }
-
-    @Override
-    public void setConverter(ParameterConverter parameterConverter) {
-        this.parameterConverter = parameterConverter;
     }
 }

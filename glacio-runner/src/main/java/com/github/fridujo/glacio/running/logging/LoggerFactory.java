b2821d9a98d3d321505e9d005a16d2a391bf39bb
ledoyen <ledoyen.loic@gmail.com>
2019-01-10 16:57:01 +0100

    Remove unused code

--- glacio-runner/src/main/java/com/github/fridujo/glacio/running/logging/LoggerFactory.java
+++ glacio-runner/src/main/java/com/github/fridujo/glacio/running/logging/LoggerFactory.java
@@ -3,9 +3,6 @@ package com.github.fridujo.glacio.running.logging;
 public abstract class LoggerFactory {
     private static final ClassLoader CLASS_LOADER = Thread.currentThread().getContextClassLoader();
 
-    private LoggerFactory() {
-    }
-
     public static Logger getLogger(Class<?> origin) {
         if (isClassPresent("org.slf4j.Logger")) {
             return new Slf4jLogger(origin);

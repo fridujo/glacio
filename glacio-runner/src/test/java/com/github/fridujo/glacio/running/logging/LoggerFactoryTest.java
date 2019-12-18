6dbe8c6d9bf6994086f77c2dc301219e6df6b2a8
ledoyen <ledoyen.loic@gmail.com>
2019-12-18 09:42:13 +0100

    Fix tests using reflection (not working since JDK-9)

--- glacio-runner/src/test/java/com/github/fridujo/glacio/running/logging/LoggerFactoryTest.java
+++ glacio-runner/src/test/java/com/github/fridujo/glacio/running/logging/LoggerFactoryTest.java
@@ -1,50 +1,25 @@
 package com.github.fridujo.glacio.running.logging;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.spy;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.net.URL;
-import java.net.URLClassLoader;
 
 import org.junit.jupiter.api.Test;
 
+import com.github.fridujo.junit.extension.classpath.ModifiedClasspath;
+
 class LoggerFactoryTest {
-
-    @Test
-    void jul_logger_is_the_default() throws NoSuchFieldException, ClassNotFoundException, IllegalAccessException {
-        ClassLoader classLoader = spy(new URLClassLoader(new URL[0], this.getClass().getClassLoader()));
-        doThrow(new ClassNotFoundException()).when(classLoader).loadClass(eq("org.slf4j.Logger"));
-
-        Field classLoaderField = LoggerFactory.class.getDeclaredField("CLASS_LOADER");
-
-        // ignore private
-        classLoaderField.setAccessible(true);
-        // ignore final
-        Field modifiersField = Field.class.getDeclaredField("modifiers");
-        modifiersField.setAccessible(true);
-        modifiersField.setInt(classLoaderField, classLoaderField.getModifiers() & ~Modifier.FINAL);
-        // replace classLoader by the mock
-        classLoaderField.set(null, classLoader);
-
-        Logger logger = LoggerFactory.getLogger(LoggerFactoryTest.class);
-
-        assertThat(logger).isExactlyInstanceOf(JulLogger.class);
-
-        logger.info("test");
-        logger.warn("test", new RuntimeException("test"));
-    }
 
     @Test
     void slf4j_is_used_if_found() {
         Logger logger = LoggerFactory.getLogger(LoggerFactoryTest.class);
 
         assertThat(logger).isExactlyInstanceOf(Slf4jLogger.class);
+    }
 
-        logger.info("test");
-        logger.warn("test", new RuntimeException("test"));
+    @Test
+    @ModifiedClasspath(excludeJars = "slf4j-api")
+    void jul_logger_is_the_default() {
+        Logger logger = LoggerFactory.getLogger(LoggerFactoryTest.class);
+
+        assertThat(logger).isExactlyInstanceOf(JulLogger.class);
     }
 }

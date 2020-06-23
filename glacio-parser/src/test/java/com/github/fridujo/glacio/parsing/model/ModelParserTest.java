19753e55323882fdc359b486afb76e3a688faa27
boddissattva <58807088+boddissattva@users.noreply.github.com>
2020-06-23 12:02:15 +0200

    fix(parser): Bypass parameter evaluation in feature when there are any parameters values

--- glacio-parser/src/test/java/com/github/fridujo/glacio/parsing/model/ModelParserTest.java
+++ glacio-parser/src/test/java/com/github/fridujo/glacio/parsing/model/ModelParserTest.java
@@ -4,6 +4,8 @@ import static java.util.Collections.emptyMap;
 import static java.util.Collections.singletonMap;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import tool.Resource;
 
@@ -17,10 +19,10 @@ import com.github.fridujo.glacio.model.Step;
 import com.github.fridujo.glacio.model.StepArgument;
 import com.github.fridujo.glacio.parsing.i18n.GherkinLanguages;
 
+@SuppressWarnings("unchecked")
 class ModelParserTest {
 
     @Test
-    @SuppressWarnings("unchecked")
     void nominal_parsing() {
         StringSource source = Resource.load("parsing_glacio_sample.feature");
         ModelParser modelParser = new ModelParser(GherkinLanguages.load());
@@ -66,5 +68,26 @@ class ModelParserTest {
         DataTable dataTable = (DataTable) navigateToStep.getArgument().get();
         assertThat(dataTable.getRows().get(0).getCells()).containsExactly("protocol", "address", "path");
         assertThat(dataTable.getRows().get(1).getCells()).containsExactly("https", "www.yourapp.com", "/login");
+    }
+
+    @Test
+    void should_parse_simple_scenario_with_xml_data_in_docstring() {
+        StringSource source = Resource.load("parsing_glacio_with_xml.feature");
+        ModelParser modelParser = new ModelParser(GherkinLanguages.load());
+
+        Feature feature = modelParser.parse(source);
+
+        assertThat(feature.getExamples()).hasSize(3);
+        assertThat(feature.getExamples().get(1).getSteps()).hasSize(2);
+        assertThat(feature.getExamples().get(1).getSteps().get(0).getArgument())
+            .usingFieldByFieldValueComparator()
+            .hasValue(new DocString(
+                Optional.of("application/xml"),
+                "<book><title>miam</title><author>chef</author></book>"));
+        assertThat(feature.getExamples().get(2).getSteps().get(0).getArgument())
+            .usingFieldByFieldValueComparator()
+            .hasValue(new DocString(
+                Optional.of("xml"),
+                "<book><title>yummy</title><author>chef</author></book>"));
     }
 }
